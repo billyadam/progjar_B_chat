35c95b249fe2be5bcfd66cc723c50ee26778cc49
billyadam <billy@iniemail.com>
2015-12-08 10:59:53 +0700

    tugas AB

--- server_gambar.py
+++ server_gambar.py
@@ -4,9 +4,9 @@ import io
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 8080)
+server_address = ('', 8080)
 sock.bind(server_address)
-sock.listen(1)
+sock.listen(10000)
 
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 
@@ -15,7 +15,7 @@ while True:
     	connection, client_address = sock.accept()
         data = connection.recv(1000)
 	nama = data.split()
-	gambar=open("halo.jpg")
+	#gambar=open("halo.jpg")
 	if nama[1]=="/1":
 		gambar= open("Kopaka.jpg")
 	elif nama[1]=="/2":
@@ -26,8 +26,8 @@ while True:
 		gambar=open("Tahu.png")
 	elif nama[1]=="/5":
 		gambar=open("gali.jpg")
-	isinyagambar =gambar.read()	
-	gambar.close()
+	isinyagambar ="halo"
+	#gambar.close()
 	http_response = "\HTTP/1.1 200 OK \n\n%s"%isinyagambar
 
 	connection.sendall(http_response)
